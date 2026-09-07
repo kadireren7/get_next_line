ecf611787dbab85bd953b7a75626043a695b10ab
Kadir Eren Altintas <kadirerenaltintas@hotmail.com.tr>
2026-09-07 20:27:26 +0300

    Add string join utility

--- get_next_line.h
+++ get_next_line.h
@@ -11,5 +11,6 @@
 char	*get_next_line(int fd);
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(const char *s1, const char *s2);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -9,6 +9,7 @@ size_t	ft_strlen(const char *s)
         i++;
     return (i);
 }
+
 char	*ft_strchr(const char *s, int c)
 {
 	int		i;
@@ -25,4 +26,29 @@ char	*ft_strchr(const char *s, int c)
 	if (s[i] == ch)
 		return ((char *)&s[i]);
 	return (NULL);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*new;
+	size_t	i;
+	size_t	j;
+
+	new = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!new)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		new[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		new[i + j] = s2[j];
+		j++;
+	}
+	new[i + j] = '\0';
+	return (new);
 }
